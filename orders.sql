487209958ed3e8343e13bd31236f279d158a330f
Hamal Samuel <hammy@Hamals-MacBook-Air.local>
2022-06-01 18:45:23 +1000

    Updated SQL

--- orders.sql
+++ orders.sql
@@ -1,22 +1,4 @@
 
--- In the orders.sql file, write out the code for the following problems:
-
--- Create a table called orders that records: order_id, person_id, product_name, product_price, quantity.
-
--- Add 5 orders to the orders table.
-
--- Make orders for at least two different people.
-
--- person_id should be different for different people.
-
--- Select all the records from the orders table.
-
--- Calculate the total number of products ordered.
-
--- Calculate the total order price.
-
--- Calculate the total order price by a single person_id.
-
 CREATE TABLE records (
     orderID int,
     personID int,
